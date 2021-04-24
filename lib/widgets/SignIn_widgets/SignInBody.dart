c5d0906973f631a75369516f61bb85dacff0dbd8
Ornny <61090037@kmitl.ac.th>
2021-04-25 01:23:48 +0700

    get value from textfield

--- lib/widgets/SignIn_widgets/SignInBody.dart
+++ lib/widgets/SignIn_widgets/SignInBody.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/screens/Otp/otp_page.dart';
 
-import 'SignForm.dart';
 import 'SignInHeader.dart';
 
-class SignInBody extends StatelessWidget {
+class SignInBody extends StatefulWidget {
+  @override
+  _SignInBodyState createState() => _SignInBodyState();
+}
+
+class _SignInBodyState extends State<SignInBody> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    TextEditingController usernameController = new TextEditingController();
+    TextEditingController passwordController = new TextEditingController();
 
     return Scaffold(
       body: Container(
@@ -20,11 +26,70 @@ class SignInBody extends StatelessWidget {
             children: [
               SignInHeader(),
               SizedBox(height: 30),
-              SignForm(),
-              SizedBox(height: 30),
+              // SignForm(),
+              TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: "username",
+                  hintText: "enter username",
+                  filled: true,
+                  fillColor: Color(0xFFFFECDF),
+                  suffixIcon: Icon(Icons.person),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Color(0xFF757575),
+                    ),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Color(0xFF757575),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "password",
+                  suffixIcon: Icon(Icons.lock),
+                  hintText: "enter password",
+                  filled: true,
+                  fillColor: Color(0xFFFFECDF),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Color(0xFF757575),
+                    ),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Color(0xFF757575),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
               RaisedButton(
                 onPressed: () => {
                   Navigator.pushNamed(context, OtpScreen.routeName),
+                  print(usernameController.text.toString()),
+                  print(passwordController.text.toString()),
                 },
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30)),
